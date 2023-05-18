083e76cc7ab808c6e31e9a4f98a5af40805da44a
chenzhipeng <63012138@qq.com>
2023-05-19 00:24:56 +0800

    deploy

--- plugins/vuepress-tabs/index.js
+++ plugins/vuepress-tabs/index.js
@@ -1,5 +1,13 @@
 const { tabs, tabpane } = require('./utils');
+
+const importStr = `import {Tabs, TabPane} from 'element-ui';
+                   export default({Vue}) => {
+                      Vue.use(Tabs);
+                      Vue.use(TabPane);
+                   }`;
+
 module.exports = {
+  enhanceAppFiles: importStr,
   extendMarkdown: (md) => {
     tabs(md);
     tabpane(md);
